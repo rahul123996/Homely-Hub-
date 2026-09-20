3e20ea38aaf7cc6e20e42b6f189159e0102c2215
rahul123996 <rahulprajapati25708@gmail.com>
2026-09-20 17:09:17 +0530

    second backend deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      '/api':{
-        target: ' https://homely-hub-project.onrender.com ',
+    proxy: {
+      '/api': {
+        target: "https://homely-hub-project.onrender.com",
         changeOrigin: true,
         secure: false,
       }
