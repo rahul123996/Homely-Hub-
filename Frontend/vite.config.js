c728f0c29b4d6a0357e099de18f84646ee3b17b3
rahul123996 <rahulprajapati25708@gmail.com>
2026-09-20 14:29:33 +0530

    depoly backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8000',
+        target: ' https://homely-hub-project.onrender.com ',
         changeOrigin: true,
         secure: false,
       }
